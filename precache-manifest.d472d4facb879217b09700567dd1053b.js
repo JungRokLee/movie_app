7eab94897c68a9d54d7ceeb22c3c56cd45a77848
Jung Rok Lee <jungroklee@Jungui-MacBook-Pro.local>
2019-07-10 15:17:33 +0900

    Updates

---
+++ precache-manifest.d472d4facb879217b09700567dd1053b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40891815e877ac22d6f2d2e105f45d6d",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "29f8471400d4b5272832",
+    "url": "/movie_app/static/css/main.03ba641c.chunk.css"
+  },
+  {
+    "revision": "a0d9c7aa34de5fee1f39",
+    "url": "/movie_app/static/js/2.ea5cb7b5.chunk.js"
+  },
+  {
+    "revision": "29f8471400d4b5272832",
+    "url": "/movie_app/static/js/main.47de342c.chunk.js"
+  },
+  {
+    "revision": "7952d9d4b252ca1c580e",
+    "url": "/movie_app/static/js/runtime~main.e6fc6960.js"
+  }
+]);
